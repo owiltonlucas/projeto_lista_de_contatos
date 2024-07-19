3951799e68dace3ef9a967fa1456cf10dcc3030c
owiltonlucas <wiltonlucas.owl@gmail.com>
2024-07-19 20:57:14 -0300

    Primeiro commit

---
+++ src/routes/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import { createContact, deleteContact, getContacts } from '../services/contact';
+
+const router = express.Router();
+
+router.post('/contato', async (req, res) => {
+    const { name } = req.body;
+
+    if(!name || name.length < 2) {
+        return res.json({ error: 'Nome precisa ter pelo menos 2 caracteres.' });
+    }
+
+    await createContact(name);
+
+    res.status(201).json({ contato: name });
+});
+
+router.get('/contatos', async (req, res) => {
+    let list = await getContacts();
+    res.json({ contatos: list });
+});
+
+router.delete('/contato', async (req, res) => {
+    const { name } = req.query;
+
+    if(!name) {
+        return res.json({ error: 'Precisa mandar um nome para excluir.' });
+    }
+
+    await deleteContact(name as string);
+
+    res.json({ contato: name });
+});
+
+export default router;
